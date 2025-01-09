f8169c20376482329a7035cdfbb3e44e65c31da3
Giaccaglia99 <giaccaglial@gmail.com>
2025-01-09 09:27:12 -0300

    primer repositorio

---
+++ frontend/app.js
@@ -0,0 +1,15 @@
+var asociate = document.getElementById('boton-asociate');
+var horarios = document.getElementById('boton-horarios');
+
+asociate.addEventListener('click', function (e) {
+    window.location.href = "./registro.html";
+});
+
+
+
+horarios.addEventListener('click',function openModal ()   {
+    document.getElementById('modal'). style.display = 'flex';
+});
+function closeModal () {
+    document.getElementById('modal').style.display = 'none';
+};
