7f674a6a7d4d25b74b8a23f30b543dc94e4b34e6
Giaccaglia99 <giaccaglial@gmail.com>
2025-01-13 06:06:41 -0300

    cambio en la esctructura de las cards de servicios

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,13 @@ var asociate = document.getElementById('boton-asociate');
 var horarios = document.getElementById('boton-horarios');
 
 asociate.addEventListener('click', function (e) {
-    window.location.href = "./registro.html";
+    window.location.href = "../registro/index.html";
 });
 
 
 
 horarios.addEventListener('click',function openModal ()   {
-    document.getElementById('modal'). style.display = 'flex';
+    document.getElementById('modal').style.display = 'flex';
 });
 function closeModal () {
     document.getElementById('modal').style.display = 'none';
